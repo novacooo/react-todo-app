88bf9fec4421bede2728940995b5b3ce5834fb0d
novacooo <jaceknowak226@gmail.com>
2021-09-18 22:52:08 +0200

    Sidebar animation performance fixes

--- src/components/organisms/Sidebar/Sidebar.tsx
+++ src/components/organisms/Sidebar/Sidebar.tsx
@@ -23,14 +23,15 @@ const Container = styled.div<ISidebarProps>`
   visibility: ${({ active }) => (active ? 'visible' : 'hidden')};
   position: fixed;
   top: 0;
-  left: ${({ active }) => (active ? '0' : '-85%')};
+  left: 0;
   z-index: 2;
-  width: 85%;
+  width: 80%;
   height: 100%;
   background-color: ${({ theme }) => theme.BG_SECONDARY};
   box-shadow: ${({ theme }) => theme.DIALOG_SHADOW};
   border-right: 4px solid ${({ theme }) => theme.MAIN_BORDER};
-  transition: visibility 0.6s, left 0.6s;
+  transform: translateX(${({ active }) => (active ? '0' : '-100%')});
+  transition: visibility 0.6s, transform 0.6s;
 `;
 
 const Sidebar = ({ active = false, onClickBg }: ISidebarProps): JSX.Element => {
