3381abbbde8ed6726da7cf299eae18ae610b11b8
novacooo <jaceknowak226@gmail.com>
2021-09-19 23:47:05 +0200

    Layout improvements on LoginPage

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 import { ReactComponent as AppLogo } from 'assets/logo/app_logo.svg';
 import { ReactComponent as EmailIcon } from 'assets/icons/email.svg';
 import { ReactComponent as LockIcon } from 'assets/icons/lock.svg';
+import { ReactComponent as ArrowRightIcon } from 'assets/icons/arrow_right.svg';
 import PageTemplate from 'templates/PageTemplate';
 import Button from 'components/atoms/Button/Button';
 import { Link } from 'react-router-dom';
 import { routes } from 'routes';
 import Input from 'components/atoms/Input/Input';
+import { fontSettings } from 'theme/fontSettings';
 
 const Container = styled.div`
   display: flex;
@@ -17,27 +19,43 @@ const Container = styled.div`
 `;
 
 const StyledAppLogo = styled(AppLogo)`
-  margin-top: 160px;
+  margin-top: 140px;
   width: 180px;
   height: auto;
   fill: ${({ theme }) => theme.MAIN};
 `;
 
-const InputsContainer = styled.div`
-  margin-top: 50px;
+const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  margin-top: 80px;
+  gap: 40px;
   width: 100%;
   max-width: 400px;
 `;
 
+const Header = styled.h2`
+  color: ${({ theme }) => theme.MAIN};
+  font-size: ${fontSettings.sizesDesktop.xl};
+  font-weight: ${fontSettings.weights.semiBold};
+`;
+
+const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+`;
+
 const StyledLink = styled(Link)`
+  align-self: center;
+  width: 100%;
+  max-width: 220px;
   text-decoration: none;
 `;
 
 const StyledButton = styled(Button)`
-  margin-top: 50px;
+  width: 100%;
 `;
 
 const LoginPage = (): JSX.Element => {
@@ -45,13 +63,16 @@ const LoginPage = (): JSX.Element => {
     <PageTemplate>
       <Container>
         <StyledAppLogo />
-        <InputsContainer>
-          <Input icon={EmailIcon} placeholder="Email address" />
-          <Input type="password" icon={LockIcon} placeholder="Password" />
-        </InputsContainer>
-        <StyledLink to={routes.home}>
-          <StyledButton>Go to HomePage</StyledButton>
-        </StyledLink>
+        <FormContainer>
+          <Header>Sign In</Header>
+          <InputsContainer>
+            <Input icon={EmailIcon} placeholder="Email address" />
+            <Input type="password" icon={LockIcon} placeholder="Password" />
+          </InputsContainer>
+          <StyledLink to={routes.home}>
+            <StyledButton icon={ArrowRightIcon}>Login</StyledButton>
+          </StyledLink>
+        </FormContainer>
       </Container>
     </PageTemplate>
   );
