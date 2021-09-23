40e2a85fe8a70bcfe300ad5ab87f5b08eff92d50
novacooo <jaceknowak226@gmail.com>
2021-09-23 23:05:21 +0200

    Added TODO in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root';
 
+// TODO: Mobile components
+
 ReactDOM.render(<Root />, document.getElementById('root'));
