b0d4fd517daa2129e1c13a3a8e22e7bbecd31677
novacooo <jaceknowak226@gmail.com>
2021-10-01 17:14:39 +0200

    Moved CategoriesBar to separated component

---
+++ src/components/molecules/CategoriesBar/CategoriesBar.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from 'components/atoms/Button/Button';
+import CategoryButton from 'components/atoms/CategoryButton/CategoryButton';
+import { ReactComponent as AddIcon } from 'assets/icons/add.svg';
+import { fontSettings } from 'theme/fontSettings';
+import { BP_MOBILE_MAX, BP_TABLET_MAX, BP_TABLET_MIN } from 'app_constants';
+
+const CategoriesContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+
+const StyledAddButton = styled(Button)`
+  height: auto;
+  padding: 12px 24px;
+  background-color: ${({ theme }) => theme.BG_PRIMARY};
+  font-size: ${fontSettings.sizesDesktop.s};
+  color: ${({ theme }) => theme.MAIN};
+
+  &:hover,
+  &:focus {
+    background-color: ${({ theme }) => theme.MAIN_TRANSPARENT};
+  }
+
+  @media (min-width: ${BP_TABLET_MIN}) and (max-width: ${BP_TABLET_MAX}) {
+    padding: 11px 22px;
+  }
+
+  @media (max-width: ${BP_MOBILE_MAX}) {
+    padding: 10px 18px;
+    font-size: ${fontSettings.sizesMobile.s};
+  }
+`;
+
+const CategoriesBar = (): JSX.Element => {
+  return (
+    <CategoriesContainer>
+      <CategoryButton active>All categories</CategoryButton>
+      <CategoryButton>Work</CategoryButton>
+      <CategoryButton>School</CategoryButton>
+      <CategoryButton>House</CategoryButton>
+      <StyledAddButton icon={AddIcon} iconSize={12} iconMargin={12} position="left">
+        Add Category
+      </StyledAddButton>
+    </CategoriesContainer>
+  );
+};
+
+export default CategoriesBar;
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Button from 'components/atoms/Button/Button';
-import CategoryButton from 'components/atoms/CategoryButton/CategoryButton';
 import styled from 'styled-components';
 import PageTemplate from 'templates/PageTemplate';
 import { ReactComponent as LoupeIcon } from 'assets/icons/loupe.svg';
 import { ReactComponent as AddIcon } from 'assets/icons/add.svg';
 import { ReactComponent as FilterIcon } from 'assets/icons/filter.svg';
 import { ReactComponent as DropDownIcon } from 'assets/icons/drop_down.svg';
-import { fontSettings } from 'theme/fontSettings';
 import { BP_MOBILE_MAX, BP_TABLET_MAX, BP_TABLET_MIN } from 'app_constants';
 import Input from 'components/atoms/Input/Input';
+import CategoriesBar from 'components/molecules/CategoriesBar/CategoriesBar';
 
 const Container = styled.div`
   display: flex;
@@ -25,36 +24,6 @@ const Container = styled.div`
 
   @media (max-width: ${BP_MOBILE_MAX}) {
     padding: 94px 0;
-  }
-`;
-
-const CategoriesContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 10px;
-`;
-
-const StyledAddButton = styled(Button)`
-  height: auto;
-  padding: 12px 24px;
-  background-color: ${({ theme }) => theme.BG_PRIMARY};
-  font-size: ${fontSettings.sizesDesktop.s};
-  color: ${({ theme }) => theme.MAIN};
-
-  &:hover,
-  &:focus {
-    background-color: ${({ theme }) => theme.MAIN_TRANSPARENT};
-  }
-
-  @media (min-width: ${BP_TABLET_MIN}) and (max-width: ${BP_TABLET_MAX}) {
-    padding: 11px 22px;
-  }
-
-  @media (max-width: ${BP_MOBILE_MAX}) {
-    padding: 10px 18px;
-    font-size: ${fontSettings.sizesMobile.s};
   }
 `;
 
@@ -79,15 +48,7 @@ const HomePage = (): JSX.Element => {
   return (
     <PageTemplate>
       <Container>
-        <CategoriesContainer>
-          <CategoryButton active>All categories</CategoryButton>
-          <CategoryButton>Work</CategoryButton>
-          <CategoryButton>School</CategoryButton>
-          <CategoryButton>House</CategoryButton>
-          <StyledAddButton icon={AddIcon} iconSize={12} iconMargin={12} position="left">
-            Add Category
-          </StyledAddButton>
-        </CategoriesContainer>
+        <CategoriesBar />
         <MenuContainer>
           <StyledInput placeholder="Search for notes..." icon={LoupeIcon} />
           <ButtonsContainer>
