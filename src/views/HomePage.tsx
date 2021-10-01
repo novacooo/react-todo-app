3d317ca9653832b7bdde6f298a13e4104c8c532c
novacooo <jaceknowak226@gmail.com>
2021-10-01 17:17:24 +0200

    Moved MenuBar to separated component

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import Button from 'components/atoms/Button/Button';
 import styled from 'styled-components';
 import PageTemplate from 'templates/PageTemplate';
-import { ReactComponent as LoupeIcon } from 'assets/icons/loupe.svg';
-import { ReactComponent as AddIcon } from 'assets/icons/add.svg';
-import { ReactComponent as FilterIcon } from 'assets/icons/filter.svg';
-import { ReactComponent as DropDownIcon } from 'assets/icons/drop_down.svg';
 import { BP_MOBILE_MAX, BP_TABLET_MAX, BP_TABLET_MIN } from 'app_constants';
-import Input from 'components/atoms/Input/Input';
 import CategoriesBar from 'components/molecules/CategoriesBar/CategoriesBar';
+import MenuBar from 'components/molecules/MenuBar/MenuBar';
 
 const Container = styled.div`
   display: flex;
@@ -27,40 +22,12 @@ const Container = styled.div`
   }
 `;
 
-// TODO: Add responsivity for menu
-
-const MenuContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-`;
-
-const StyledInput = styled(Input)`
-  max-width: 500px;
-`;
-
-const ButtonsContainer = styled.div`
-  display: flex;
-  gap: 20px;
-`;
-
 const HomePage = (): JSX.Element => {
   return (
     <PageTemplate>
       <Container>
         <CategoriesBar />
-        <MenuContainer>
-          <StyledInput placeholder="Search for notes..." icon={LoupeIcon} />
-          <ButtonsContainer>
-            <Button icon={AddIcon}>New Item</Button>
-            <Button secondary icon={FilterIcon} iconSize={18}>
-              Filter
-            </Button>
-            <Button secondary icon={DropDownIcon} iconSize={10}>
-              Board
-            </Button>
-          </ButtonsContainer>
-        </MenuContainer>
+        <MenuBar />
       </Container>
     </PageTemplate>
   );
