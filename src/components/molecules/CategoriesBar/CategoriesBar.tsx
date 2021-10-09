8d668f6b1bcbada644eca791b3393f2f549bfad8
novacooo <jaceknowak226@gmail.com>
2021-10-09 21:51:39 +0200

    Fixed centering and scrolling for categories bar

--- src/components/molecules/CategoriesBar/CategoriesBar.tsx
+++ src/components/molecules/CategoriesBar/CategoriesBar.tsx
@@ -4,25 +4,25 @@ import Button from 'components/atoms/Button/Button';
 import CategoryButton from 'components/atoms/CategoryButton/CategoryButton';
 import { ReactComponent as AddIcon } from 'assets/icons/add.svg';
 import { fontSettings } from 'theme/fontSettings';
-import {
-  BP_MOBILE_MAX,
-  BP_TABLET_MAX,
-  BP_TABLET_MIN,
-  MARGIN_SIDE_MOBILE,
-  MARGIN_SIDE_TABLET,
-} from 'app_constants';
+import { BP_MOBILE_MAX, BP_TABLET_MAX, BP_TABLET_MIN, MARGIN_SIDE_MOBILE } from 'app_constants';
 
 interface ICategoriesBarProps {
   showAddButton?: boolean;
 }
 
 const BarContainer = styled.div`
-  margin: 0 auto;
+  width: 100%;
 
   @media (max-width: ${BP_MOBILE_MAX}) {
-    position: relative;
-    left: -${MARGIN_SIDE_MOBILE};
+    padding: 0 ${MARGIN_SIDE_MOBILE};
     width: calc(100% + ${MARGIN_SIDE_MOBILE} + ${MARGIN_SIDE_MOBILE});
+
+    overflow-x: scroll;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    &::-webkit-scrollbar {
+      display: none;
+    }
   }
 `;
 
@@ -32,20 +32,13 @@ const ButtonsContainer = styled.div`
   align-items: center;
   flex-wrap: wrap;
   gap: 10px;
-  width: 100%;
 
   @media (max-width: ${BP_MOBILE_MAX}) {
-    justify-content: center;
+    justify-content: flex-start;
     flex-wrap: nowrap;
-    padding: 0 ${MARGIN_SIDE_MOBILE};
-
-    overflow-x: scroll;
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-
-    &::-webkit-scrollbar {
-      display: none;
-    }
+    margin: 0 auto;
+    padding-right: ${MARGIN_SIDE_MOBILE};
+    width: max-content;
   }
 `;
 
