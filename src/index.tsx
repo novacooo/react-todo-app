469386053e0c3677279bd7a09a0c9b65a37e8068
novacooo <jaceknowak226@gmail.com>
2021-10-10 12:15:35 +0200

    Checked TODO's v2

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root';
 
-// TODO: Mobile components
-
 ReactDOM.render(<Root />, document.getElementById('root'));
